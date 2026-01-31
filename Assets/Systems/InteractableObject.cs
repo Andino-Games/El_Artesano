a8ea7b5b2bfbf9dffc335e5260100a2571e66c09
SantiagoAc07 <153125231+SinDinero-gif@users.noreply.github.com>
2026-01-30 21:42:46 -0500

    Interactable M echanic Et1

---
+++ Assets/Systems/InteractableObject.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private Material outlineMaterial;
+    private Material _originalMaterial;
+
+    private void Start()
+    {
+        _originalMaterial = meshRenderer.material;
+    }
+
+    public void ToggleOutline(bool show)
+    {
+        meshRenderer.enabled = show ? outlineMaterial : _originalMaterial; 
+    }
+}
---
+++ Assets/Systems/Player/PlayerCollisionController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerCollisionController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider puzzle)
+    {
+        // Check if the object we bumped into is interactable
+        if (puzzle.TryGetComponent<InteractableObject>(out var interactable))
+        {
+            interactable.ToggleOutline(true);
+            Debug.Log("Interactable Reached");
+        }
+    }
+
+    private void OnTriggerExit(Collider puzzle)
+    {
+        if (puzzle.TryGetComponent<InteractableObject>(out var interactable))
+        {
+            interactable.ToggleOutline(false);
+            Debug.Log("Interactable Farther");
+        }
+    }
+}
