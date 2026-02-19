ddf244da31bf13e099423e43431ac4dc6788644f
Jose-Agudelo-LITG <46972134+NothigBox@users.noreply.github.com>
2026-02-18 23:29:26 -0500

    Short audios

--- Assets/Scripts/Audio/SceneMusicSetter.cs
+++ Assets/Scripts/Audio/SceneMusicSetter.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SceneMusicSetter : MonoBehaviour
 {
-    [SerializeField] private LevelMusic music = LevelMusic.Level_Gameplay;
+    [SerializeField] private LevelMusic music = LevelMusic.Level_Introduction;
     [SerializeField] private float fadeDuration = 0.5f;
 
     private void Start()
